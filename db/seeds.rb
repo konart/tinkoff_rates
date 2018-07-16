c67ccfd6b75c9845e054029f8dc914c6154e48b6
Artem Kondratev <konapt@gmail.com>
2018-07-16 09:48:57 +0300

    Added seeds to populate db with randomized rates values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+time = 1.day.ago
+rng = Random.new
+
+24.times do |_|
+  time += 1.hour
+  sell = rng.rand(62.0...65.0)
+  buy = rng.rand(60.0...63.0)
+  Rate.create(from: 'USD', to: 'RUB', sell: sell, buy: buy, created_at: time)
+  sell = rng.rand(82.0...85.0)
+  buy = rng.rand(80.0...83.0)
+  Rate.create(from: 'EUR', to: 'RUB', sell: sell, buy: buy, created_at: time)
+end
